22786b8490c8c0444c1969880e2f99242d9bd1ca
AniRunix <romus993@gmail.com>
2026-08-21 13:05:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def calculate_message(saved_date_str: str) -> str:
     delta_days = (today - start_date).days
     days_word = get_days_word(delta_days)
 
-    return f"привет кисуля💕💕 сегодня нам {delta_days} {days_word}! Люблю тебя, котя💕❤️"
+    return f"привет кисуля💕💕 сегодня нам должно было быть {delta_days} {days_word}! Люблю тебя, котя💕❤️"
 
 # --- Фоновая рассылка в 00:00 по МСК+3 ---
 
